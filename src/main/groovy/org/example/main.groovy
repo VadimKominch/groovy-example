f39cf5c421ec8a8078034bc8877639a423c4e076
Vadim Kominch <vadimkominch@innowise.com>
2025-10-07 14:45:56 +0200

    feat: add binding to method

--- src/main/groovy/org/example/main.groovy
+++ src/main/groovy/org/example/main.groovy
@@ -10,25 +10,34 @@ class Person {
     }
 }
 
-    println("Hello,world!")
-    /**
-     * var - evualated when it is assigned
-     * def evaluated when it is called
-     * use var for variables and def for function expression or declaration
-     * */
-    var numbers = [1,2,3,4,5]
+println("Hello,world!")
+/**
+ * var - evaluated when it is assigned
+ * def evaluated when it is called
+ * use var for variables and def for function expression or declaration
+ * */
+var numbers = [1,2,3,4,5]
+numbers << 6
+numbers.forEach {println it}
 
-    numbers.forEach {println it}
+def squares = numbers.collect { it*it}
 
-    def squares = numbers.collect { it*it}
-
-    def person = [name: "Alice", age: 30]
-    person.surname = "Smith"
-    def alice = new Person()
-    def bob = new Person()
-    alice.metaClass.y = "Alice"
-    alice.test()
+def person = [name: "Alice", age: 30]
+person.surname = "Smith"
+def alice = new Person()
+def bob = new Person()
+alice.metaClass.y = "Alice"
+alice.test()
 //    bob.test()
-    def letters = 'a'..'e'
-binding
+def letters = 'a'..'e'
 
+(0..5).each {println it}
+
+new File("exmaple.txt").eachLine {println it}
+
+binding.setVariable("a", 10)
+def f (){
+    def a = (int)binding.getVariable("a")
+    return a + 10
+}
+println(f())
